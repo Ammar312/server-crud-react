eb331cd1baf6b42081a07b53bbd3cef062cb8b8e
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-09-11 07:30:14 +0500

    api update

--- web/src/components/CreatePost.jsx
+++ web/src/components/CreatePost.jsx
@@ -5,7 +5,7 @@ import EditPostComp from "./EditPostComp";
 import { message } from "antd";
 
 const CreatePost = () => {
-  const baseURL = "http://localhost:3000";
+  // const baseURL = "http://localhost:3000";
   const [allPosts, setAllPosts] = useState([]);
   const [toggleRefresh, setToggleRefresh] = useState(false);
   const [confirmLoading, setConfirmLoading] = useState(false);
@@ -14,7 +14,7 @@ const CreatePost = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`${baseURL}/api/v1/posts`);
+        const response = await axios.get(`api/v1/posts`);
         setAllPosts(response.data);
       } catch (error) {}
     };
@@ -23,7 +23,7 @@ const CreatePost = () => {
   const submitPost = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post(`${baseURL}/api/v1/post`, {
+      const response = await axios.post(`api/v1/post`, {
         title: titleInput.current.value,
         text: bodyInput.current.value,
       });
@@ -40,7 +40,7 @@ const CreatePost = () => {
   };
   const deleteHandle = async (id) => {
     try {
-      const response = await axios.delete(`${baseURL}/api/v1/post/${id}`);
+      const response = await axios.delete(`api/v1/post/${id}`);
       console.log(response.data);
       setConfirmLoading(true);
       message.success(`${response.data}`);
@@ -65,7 +65,7 @@ const CreatePost = () => {
     const text = e.target.parentElement.previousElementSibling.lastChild.value;
 
     try {
-      const response = await axios.put(`${baseURL}/api/v1/post/${id}`, {
+      const response = await axios.put(`api/v1/post/${id}`, {
         title: title,
         text: text,
       });
